02fb812ae980f20612baf72a85013a37789bc6de
ShadowWalker2014 <kai.jiabo.feng@gmail.com>
2025-05-09 17:22:53 +0000

    Fixed UserDetail component import error
    • Created missing /src/pages directory
    • Created UserDetail.tsx with correct import paths (@/components/ui/alert-dialog)
    • Updated App.tsx to import and use the UserDetail component
    • Ensured path aliases are properly configured

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { UserDetail } from './pages/UserDetail'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-background">
+      <UserDetail />
+    </div>
+  )
+}
+
+export default App
